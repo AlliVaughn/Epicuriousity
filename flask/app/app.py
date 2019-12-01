eb65f9531923e3607b5d16691977f03514d22d6d
scross7 <sarahecross7@gmail.com>
2019-12-01 15:25:38 -0600

    trying to add sqlite

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,24 +1,46 @@
-import json
-from flask import Flask, render_template,jsonify
+import numpy as np
 import pandas as pd
-# from flask_sqlalchemy import SQLAlchemy
+import json
+import sqlalchemy as db
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify, render_template
+from sqlalchemy import MetaData
 
 app = Flask(__name__)
-###################################
-#DATABASE SETUP
-###################################
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/epicuriousity.sqlite"
-# db = SQLAlchemy(app)
 
+# # Path to sqlite
+# # database_path = "epi_db.sqlite"
 
-# reflect an existing database into a new model
-# Base = automap_base()
-# reflect the tables
-# Base.prepare(db.engine, reflect=True)
+# # Create an engine that can talk to the database
+# engine = create_engine(f"sqlite:///data/epi-db.sqlite")
+# conn = engine.connect()
 
-# Save references to each table
-# Samples_Metadata = Base.classes.sample_metadata
-# Samples = Base.classes.samples
+# # reflect the tables
+# m = MetaData()
+# m.reflect(engine)
+# epifactor = db.Table('epifactor', m, autoload=True, autoload_with=engine)
+
+# # Select the table to query
+# query = db.select([epifactor])
+# ResultProxy = conn.execute(query)
+# ResultSet = ResultProxy.fetchall()
+
+# # Send quesry results from table to a dataframe
+# ResultSet = pd.DataFrame(ResultSet)
+
+# # Drop column "0" and row 0 (not importing index and column names?), resset index to start a 0 and rename columns
+# ResultSet.drop([0], inplace=True)
+# ResultSet.drop(0, axis=1, inplace=True)
+# ResultSet.reset_index(inplace=True)
+# ResultSet.rename(columns={1:'Calories', 2:'Fat', 3:'Protein', 4:'Ingredients'}, inplace=True)
+
+# data = [{'name': 'Calories', 'value': ResultSet['Calories'][0]},
+#                    {'name': 'Fat', 'value': ResultSet['Fat'][0]},
+#                    {'name': 'Protein', 'value': ResultSet['Protein'][0]},
+#                    {'name': 'Ingredients', 'value': ResultSet['Ingredients'][0]},
+#                    {'name': 'Nothing', 'value': 0.2}] 
 
 @app.route('/')
 def index():
